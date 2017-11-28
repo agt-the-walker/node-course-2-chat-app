7f372b11f6fc82cf86b0094ccf825234416050b9
Jerome Zago <jerome.zago@gmail.com>
2017-11-28 18:57:05 +0100

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -13,13 +13,14 @@ app.use(express.static(path.join(__dirname, '..', 'public')))
 io.on('connection', socket => {
   console.log('New user connected')
 
-  socket.emit('newMessage', {
-    from: 'John',
-    text: 'See you then',
-    createdAt: 123123
+  socket.on('createMessage', message => {
+    console.log('Create message', message)
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
   })
-
-  socket.on('createMessage', message => console.log('Create message', message))
 
   socket.on('disconnect', () => console.log('User was disconnected'))
 })
