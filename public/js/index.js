3e035a44b9b5a99700da5abb66b1729997a476f3
Jerome Zago <jerome.zago@gmail.com>
2017-12-10 14:41:26 +0100

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -23,11 +23,14 @@ socket.on('newLocationMessage', message => {
 
 jQuery('#message-form').on('submit', (e) => {
   e.preventDefault()
+  const messageTextbox = jQuery('[name=message]')
 
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
-  }, data => console.log('Got it', data))
+    text: messageTextbox.val()
+  }, () => 
+    messageTextbox.val('')
+  )
 })
 
 const locationButton = jQuery('#send-location')
@@ -35,12 +38,18 @@ locationButton.on('click', () => {
   if (!navigator.geolocation)
     return alert('Geolocation not supported by your browser.')
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...')
+
   navigator.geolocation.getCurrentPosition(position => {
+    locationButton.removeAttr('disabled').text('Send location')
+
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude,
     })
   }, () => {
+    locationButton.removeAttr('disabled').text('Send location')
+
     alert('Unable to fetch location.')
   })
 })
